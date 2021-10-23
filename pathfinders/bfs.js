71bdc7d09a43998adb4a4e56e0716388c105f975
MarcosVB <mvberte@hotmail.com>
2021-10-22 22:05:25 -0300

    Refactored bfs algorithm

--- pathfinders/bfs.js
+++ pathfinders/bfs.js
@@ -1,21 +1,25 @@
 
 function solveBFS() {
+
     let steps = 0;
-    let n, n2;
+
     while (openNodes.length > 0) {
+        
         steps++;
-        n = openNodes.shift(0);
-        if (n.isTarget) {
+        let currentNode = openNodes.shift(0);
+
+        if (currentNode.isTarget) {
             console.log(`Total steps (BFS): ${steps}`);
-            printTrace(n);
+            printTrace(currentNode);
             return;
         }
-        for (let neighbor of n.neighbors) {
-            n2 = neighbor;
-            if (!closedNodes.includes(n2)) {
-                n2.previous = n;
-                openNodes.push(n2);
-                closedNodes.push(n2);
+
+        for (let neighbor of currentNode.neighbors) {
+
+            if (!closedNodes.includes(neighbor)) {
+                neighbor.previous = currentNode;
+                openNodes.push(neighbor);
+                closedNodes.push(neighbor);
             }
         }
     }
